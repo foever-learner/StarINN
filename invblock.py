3609f4186a682c16b45e708f71af759c9e19c0e6
挽今 <69063636+foever-learner@users.noreply.github.com>
2025-06-28 17:44:46 +0800

    Add files via upload

---
+++ invblock.py
@@ -0,0 +1,218 @@
+import numbers
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import config as c
+from einops import rearrange
+from math import exp
+from stardenseblock import ResidualDenseBlock_out, StarDenseBlock, StarDenseBlock_s
+
+def to_3d(x):
+    return rearrange(x, 'b c h w -> b (h w) c')
+
+def to_4d(x, h, w):
+    return rearrange(x, 'b (h w) c -> b c h w', h=h, w=w)
+
+class BiasFree_LayerNorm(nn.Module):
+    def __init__(self, normalized_shape):
+        super(BiasFree_LayerNorm, self).__init__()
+        if isinstance(normalized_shape, numbers.Integral):
+            normalized_shape = (normalized_shape,)
+        normalized_shape = torch.Size(normalized_shape)
+        assert len(normalized_shape) == 1
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.normalized_shape = normalized_shape
+
+    def forward(self, x):
+        sigma = x.var(-1, keepdim=True, unbiased=False)
+        return x / torch.sqrt(sigma + 1e-5) * self.weight
+
+class WithBias_LayerNorm(nn.Module):
+    def __init__(self, normalized_shape):
+        super(WithBias_LayerNorm, self).__init__()
+        if isinstance(normalized_shape, numbers.Integral):
+            normalized_shape = (normalized_shape,)
+        normalized_shape = torch.Size(normalized_shape)
+        assert len(normalized_shape) == 1
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.normalized_shape = normalized_shape
+
+    def forward(self, x):
+        mu = x.mean(-1, keepdim=True)
+        sigma = x.var(-1, keepdim=True, unbiased=False)
+        return (x - mu) / torch.sqrt(sigma + 1e-5) * self.weight + self.bias
+
+class LayerNorm(nn.Module):
+    def __init__(self, dim, LayerNorm_type):
+        super(LayerNorm, self).__init__()
+        if LayerNorm_type == 'BiasFree':
+            self.body = BiasFree_LayerNorm(dim)
+        else:
+            self.body = WithBias_LayerNorm(dim)
+
+    def forward(self, x):
+        h, w = x.shape[-2:]
+        return to_4d(self.body(to_3d(x)), h, w)
+
+class LayerNormFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, weight, bias, eps):
+        ctx.eps = eps
+        N, C, H, W = x.size()
+        mu = x.mean(1, keepdim=True)
+        var = (x - mu).pow(2).mean(1, keepdim=True)
+        y = (x - mu) / (var + eps).sqrt()
+        ctx.save_for_backward(y, var, weight)
+        y = weight.view(1, C, 1, 1) * y + bias.view(1, C, 1, 1)
+        return y
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        eps = ctx.eps
+        N, C, H, W = grad_output.size()
+        y, var, weight = ctx.saved_variables
+        g = grad_output * weight.view(1, C, 1, 1)
+        mean_g = g.mean(dim=1, keepdim=True)
+        mean_gy = (g * y).mean(dim=1, keepdim=True)
+        gx = 1. / torch.sqrt(var + eps) * (g - y * mean_gy - mean_g)
+        return gx, (grad_output * y).sum(dim=3).sum(dim=2).sum(dim=0), grad_output.sum(dim=3).sum(dim=2).sum(dim=0), None
+
+class LayerNorm2d(nn.Module):
+    def __init__(self, channels, eps=1e-6):
+        super(LayerNorm2d, self).__init__()
+        self.register_parameter('weight', nn.Parameter(torch.ones(channels)))
+        self.register_parameter('bias', nn.Parameter(torch.zeros(channels)))
+        self.eps = eps
+
+    def forward(self, x):
+        return LayerNormFunction.apply(x, self.weight, self.bias, self.eps)
+
+## SimpleGate Module
+class SimpleGate(nn.Module):
+    def forward(self, x):
+        x1, x2 = x.chunk(2, dim=1)
+        return x1 * x2
+
+## NAFBlock Module
+class NAFBlock(nn.Module):
+    def __init__(self, c, DW_Expand=2, FFN_Expand=2, drop_out_rate=0.):
+        super().__init__()
+        dw_channel = c * DW_Expand
+        self.conv1 = nn.Conv2d(in_channels=c, out_channels=dw_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.conv2 = nn.Conv2d(in_channels=dw_channel, out_channels=dw_channel, kernel_size=3, padding=1, stride=1, groups=dw_channel,
+                               bias=True)
+        self.conv3 = nn.Conv2d(in_channels=dw_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        # Simplified Channel Attention
+        self.sca = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_channels=dw_channel // 2, out_channels=dw_channel // 2, kernel_size=1, padding=0, stride=1,
+                      groups=1, bias=True),
+        )
+        # SimpleGate
+        self.sg = SimpleGate()
+        ffn_channel = FFN_Expand * c
+        self.conv4 = nn.Conv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.conv5 = nn.Conv2d(in_channels=ffn_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.norm1 = LayerNorm2d(c)
+        self.norm2 = LayerNorm2d(c)
+        self.dropout1 = nn.Dropout(drop_out_rate) if drop_out_rate > 0. else nn.Identity()
+        self.dropout2 = nn.Dropout(drop_out_rate) if drop_out_rate > 0. else nn.Identity()
+        self.beta = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
+        self.gamma = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
+
+    def forward(self, inp):
+        x = inp
+        # First normalization and convolution operations
+        x = self.norm1(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.sg(x)           
+        x = x * self.sca(x)      
+        x = self.conv3(x)
+        x = self.dropout1(x)
+        y = inp + x * self.beta   
+        # Second normalization and convolution operations
+        x = self.conv4(self.norm2(y))
+        x = self.sg(x)           
+        x = self.conv5(x)
+        x = self.dropout2(x)
+        return y + x * self.gamma  
+
+class INV_block(nn.Module):
+    def __init__(self, subnet_constructor=StarDenseBlock, subnet_constructor2=ResidualDenseBlock_out, clamp=1, harr=True, in_1=3, in_2=3):
+        super().__init__()
+        if harr:
+            self.split_len1 = in_1 * 4
+            self.split_len2 = in_2 * 4
+        self.clamp = clamp
+        # print(self.split_len1,self.split_len2)
+        # ρ
+        self.r = subnet_constructor2(self.split_len1, self.split_len2)
+        self.Nr = NAFBlock(self.split_len1)
+        # η
+        self.y = subnet_constructor2(self.split_len1, self.split_len2)
+        self.Ny = NAFBlock(self.split_len1)
+        # φ
+        self.f = subnet_constructor(self.split_len2, self.split_len1)
+
+    def e(self, s):
+        return torch.exp(self.clamp * 2 * (torch.sigmoid(s) - 0.5))
+
+    def forward(self, x, rev=False):
+        x1, x2 = (x.narrow(1, 0, self.split_len1),
+                  x.narrow(1, self.split_len1, self.split_len2))
+        if not rev:
+            t2 = self.f(x2)
+            y1 = x1 + t2
+            s1 = self.Nr(self.r(y1))  # 通过 NAFBlock 处理 self.r 的输出
+            t1 = self.Ny(self.y(y1))  # 通过 NAFBlock 处理 self.y 的输出
+            y2 = self.e(s1) * x2 + t1
+        else:
+            s1 = self.Nr(self.r(x1))  # 通过 NAFBlock 处理 self.r 的输出
+            t1 = self.Ny(self.y(x1))  # 通过 NAFBlock 处理 self.y 的输出
+            y2 = (x2 - t1) / self.e(s1)
+            t2 = self.f(y2)
+            y1 = (x1 - t2)
+        return torch.cat((y1, y2), 1)
+
+class INV_block_old(nn.Module):
+    def __init__(self, subnet_constructor=StarDenseBlock, subnet_constructor2=ResidualDenseBlock_out, clamp=1, harr=True, in_1=3, in_2=3):
+        super().__init__()
+        if harr:
+            self.split_len1 = in_1 * 4
+            self.split_len2 = in_2 * 4
+        self.clamp = clamp
+        # ρ
+        self.r = subnet_constructor2(self.split_len1, self.split_len2)
+        # η
+        self.y = subnet_constructor2(self.split_len1, self.split_len2)
+        # φ
+        self.f = subnet_constructor(self.split_len2, self.split_len1)
+
+    def e(self, s):
+        return torch.exp(self.clamp * 2 * (torch.sigmoid(s) - 0.5))
+
+    def forward(self, x, rev=False):
+        x1, x2 = (x.narrow(1, 0, self.split_len1),
+                  x.narrow(1, self.split_len1, self.split_len2))
+        if not rev:
+            t2 = self.f(x2)
+            y1 = x1 + t2
+            s1, t1 = self.r(y1), self.y(y1)
+            y2 = self.e(s1) * x2 + t1
+        else:
+            s1, t1 = self.r(x1), self.y(x1)
+            y2 = (x2 - t1) / self.e(s1)
+            t2 = self.f(y2)
+            y1 = (x1 - t2)
+
+        return torch.cat((y1, y2), 1)
+
+if __name__ == '__main__':
+    # 假设输入图像有 3 个通道，尺寸为 64x64
+    input_tensor = torch.randn(1, 24, 64, 64)  # batch size 为 1
+    model = INV_block()
+    # 前向传播，打印每层输出的形状
+    output = model(input_tensor)
+    print(output)
